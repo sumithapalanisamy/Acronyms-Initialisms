f8f1e85b5e3d3c00970af7fdf1043627951b8714
Sumitha Palanisamy <psumitha87@gmail.com>
2016-04-12 16:19:58 -0700

    Added

---
+++ Acronyms /MyViewC.swift
@@ -0,0 +1,16 @@
+//
+//  MyViewC.swift
+//  Acronyms
+//
+//  Created by Sumitha  on 4/12/16.
+//  Copyright © 2016 Sumitha Palanisamy. All rights reserved.
+//
+
+import Cocoa
+
+class MyViewC: UIViewController {
+    func hello() -> String {
+        return "hi"
+    }
+
+}

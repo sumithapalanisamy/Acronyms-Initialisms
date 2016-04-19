ea9bb558b789619475099fbf23bc96f20b3143d0
Sumitha Palanisamy <psumitha87@gmail.com>
2016-04-19 10:33:32 -0700

    Controller

---
+++ Acronyms /SampleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SampleViewController.swift
+//  Acronyms
+//
+//  Created by Sumitha  on 4/19/16.
+//  Copyright © 2016 Sumitha Palanisamy. All rights reserved.
+//
+
+import Cocoa
+
+class SampleViewController: UIViewController {
+    var myview:MyViewC!
+    myview = nil
+
+}

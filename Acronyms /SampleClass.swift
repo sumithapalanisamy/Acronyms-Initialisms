0bb557f37be3424646fb83a6453aede7035b0f46
Sumitha Palanisamy <psumitha87@gmail.com>
2016-05-11 14:37:24 -0700

    added class

---
+++ Acronyms /SampleClass.swift
@@ -0,0 +1,21 @@
+//
+//  SampleClass.swift
+//  
+//
+//  Created by Sumitha  on 5/11/16.
+//
+//
+
+import UIKit
+
+class SampleClass: NSObject {
+    func demo() {
+        print("hello")
+        let c = "Hello"
+        
+        if ( c != nil) {
+            c = "hi"
+        }
+    }
+
+}
